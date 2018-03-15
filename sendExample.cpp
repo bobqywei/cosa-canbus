3e97b4e1d1f08bc7c7f06bd6a1237825437d710f
bobqywei <bobqywei@gmail.com>
2018-03-14 22:51:03 -0400

    Added examples

--- sendExample.cpp
+++ sendExample.cpp
@@ -16,7 +16,7 @@ void setup() {
 unsigned char stmp[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 
 void loop() {
-    // send data:  id = 0x00, standrad flame, data len = 8, stmp: data buf
+    // send data:  id = 0x00, standard frame, data len = 8, stmp: data buf
     CAN.sendMsgBuf(0x00, 0, 8, stmp);  
     delay(100);
 }
